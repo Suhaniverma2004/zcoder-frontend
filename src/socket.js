a675f688fa0264ad1dd2bc953acf8b0ed143c50a
Suhani <isuhaniverma@gmail.com>
2025-06-30 17:07:12 +0530

    Vercel added

--- src/socket.js
+++ src/socket.js
@@ -1,10 +1,5 @@
 import { io } from 'socket.io-client';
 
-// Use the same logic to determine the correct server URL
-const SERVER_URL = process.env.NODE_ENV === 'production'
-  ? process.env.REACT_APP_SOCKET_URL
-  : 'http://localhost:5001';
+const SERVER_URL = process.env.REACT_APP_SOCKET_URL || 'http://localhost:5001';
 
-export const socket = io(SERVER_URL, {
-  autoConnect: false
-});
+export const socket = io(SERVER_URL, { autoConnect: false });
